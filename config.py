4a2d84486263cae4fcc10877fb2ff039ac15fd97
Martin1024 <zsscjcjdhr@gmail.com>
2019-02-27 18:24:09 +0800

    update

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 sql_password = ''
 tolerance = 13
+""" 百度云 APPID AK SK """
+APP_ID = ''
+API_KEY = ''
+SECRET_KEY = ''
+
+AIP_MAX_LEN_TITLE = 40  # 80/2
+AIP_MAX_LEN_CONTENT = 30000  # 65535/2
+
+SENTENCES_COUNT = 5
+TZ_DELTA = 8
--- crawler.py
+++ crawler.py
@@ -26,16 +26,7 @@ from aip import AipNlp
 from config import *
 
 
-""" 百度云 APPID AK SK """
-APP_ID = '15606375'
-API_KEY = 'bcBGUHh2vuSEbKMfbUGttbwx'
-SECRET_KEY = 'BWfe4RCxslXheFthP9Gf5d8GGROGPyNT'
 
-AIP_MAX_LEN_TITLE = 40  # 80/2
-AIP_MAX_LEN_CONTENT = 30000  # 65535/2
-
-SENTENCES_COUNT = 5
-TZ_DELTA = 8
 
 
 client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
